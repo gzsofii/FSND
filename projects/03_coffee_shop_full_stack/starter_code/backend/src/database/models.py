6a6022a4cba5de4c4af8692cc5bfb3e5a4c05b30
gzsofii <gal.zsofi@gmail.com>
2021-07-02 10:55:27 +0200

    change sqllite to postgres

--- projects/03_coffee_shop_full_stack/starter_code/backend/src/database/models.py
+++ projects/03_coffee_shop_full_stack/starter_code/backend/src/database/models.py
@@ -3,9 +3,16 @@ from sqlalchemy import Column, String, Integer
 from flask_sqlalchemy import SQLAlchemy
 import json
 
-database_filename = "database.db"
+database_filename = "coffeedata"
 project_dir = os.path.dirname(os.path.abspath(__file__))
-database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+database_path = "postgresql:///{}".format(database_filename)
+
+DB_HOST = os.getenv('DB_HOST', '127.0.0.1:5432')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'postgres')
+DB_NAME = os.getenv('DB_NAME', 'coffeeshop')
+DB_PATH = 'postgresql+psycopg2://{}:{}@{}/{}'\
+            .format(DB_USER, DB_PASSWORD, DB_HOST, DB_NAME)
 
 db = SQLAlchemy()
 
@@ -16,7 +23,7 @@ setup_db(app)
 
 
 def setup_db(app):
-    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_DATABASE_URI"] = DB_PATH
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.app = app
     db.init_app(app)
@@ -39,8 +46,7 @@ def db_drop_and_create_all():
         recipe='[{"name": "water", "color": "blue", "parts": 1}]'
     )
 
-
-drink.insert()
+    drink.insert()
 # ROUTES
 
 '''
